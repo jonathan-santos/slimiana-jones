396e692aaa6be441df486b8886e9894634b1405d
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2020-08-24 22:01:50 -0300

    Added action impulse and colliding only jump to player

--- scripts/slimiana_jones.gd
+++ scripts/slimiana_jones.gd
@@ -1,14 +1,17 @@
 extends RigidBody
 
+var direction := Vector3()
 var movement_speed := 10.0
 var jump_force := 500.0
-var direction := Vector3()
+var impulse_force := 3000.0
+var is_colliding := false
 
 onready var camera := get_viewport().get_camera()
 
 func _physics_process(_delta):
 	move()
 	jump()
+	impulse()
 	
 func move():
 	direction = Vector3()
@@ -27,5 +30,17 @@ func move():
 	add_central_force(direction * movement_speed)
 
 func jump():
-	if Input.is_action_just_pressed("jump"):
+	if Input.is_action_just_pressed("jump") and is_colliding:
 		add_central_force(Vector3.UP * jump_force)
+
+func impulse():
+	if Input.is_action_just_pressed("impulse") and is_colliding:
+		add_central_force(direction * impulse_force)
+
+func _on_SlimianaJones_body_entered(body):
+	if body.is_in_group("solid"):
+		is_colliding = true
+
+func _on_SlimianaJones_body_exited(body):
+	if body.is_in_group("solid"):
+		is_colliding = false
